e2f56fbd4ab44dc7b701430b27565871ae85e5bd
qmaowu <it_accp@126.com>
2020-04-04 23:51:50 +0000

    Don't Repeat Yourself

--- lists/tests.py
+++ lists/tests.py
@@ -21,6 +21,11 @@ class HomePageTest(TestCase):
         #self.assertIn('<title>To-Do lists</title>',html)
         #self.assertTrue(html.strip().endswith('</html>'))
         self.assertTemplateUsed(response,'home.html')
+    def test_can_save_a_POST_request(self):
+        response=self.client.post('/',data={'item_text':'A new list item'})
+        #print(response.content.decode())
+        self.assertIn('A new list item',response.content.decode())
+        self.assertTemplateUsed(response,'home.html')
 
 
 '''class SmokeTest(TestCase):
