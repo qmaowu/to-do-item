8cb514970231c9bae133ffc89e0c2fa2ba44f564
qmaowu <it_accp@126.com>
2020-04-05 14:19:33 +0000

    URLs for forms x2, moved code in views + tests, new URL

--- lists/tests.py
+++ lists/tests.py
@@ -72,6 +72,20 @@ class ListViewTest(TestCase):
     '''def test_uses_list_template(self):
         response=self.client.get('lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response,'list.html')'''
+class NewListTest(TestCase):
+    
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new',data={'item_text':'A new list item'})
+        self.assertEqual(Item.objects.count(),1)
+        new_item=Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+
+    def test_redirects_after_POST(self):
+        response=self.client.post('/lists/new',data={'item_text':'A new list item'})
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
+
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
