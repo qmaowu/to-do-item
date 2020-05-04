33b46821e8e810d8d62d2b60a33297aa6da1fca5
qmaowu <it_accp@126.com>
2020-05-04 00:19:17 +0000

    Finial fetch cat

---
+++ fun_tests.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+
+browser=webdriver.Firefox()
+brower.get("http://127.0.0.1:8000")
--- lists/views.py
+++ lists/views.py
@@ -1,43 +1,22 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from lists.models import Item,List
-# Create your views here.
+from django.shortcuts import redirect, render
+from lists.models import Item, List
 
-#home_page=None
 def home_page(request):
-    '''item=Item()
-    item.text=request.POST.get('item.text','')
-    item.save()'''
-    if request.method=='POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-        #new_item_text=request.POST['item_text']
-        #Item.objects.create(text=new_item_text)
-    #else:
-        #new_item_text=''
-    #pass
-    #return HttpResponse('<html><title>To-Do lists</title></html>')
-    #if request.method=='POST':
-        #return HttpResponse(request.POST['item_text'])
-    '''return render(request,'home.html',{
-        #'new_item_text':request.POST.get('item_text',''),
-        'new_item_text':new_item_text,
-    })'''
-    #items=Item.objects.all()
-    return render(request,'home.html')
+    return render(request, 'home.html')
 
-def view_list(request,list_id):
-    list_=List.objects.get(id=list_id)
-    #items=Item.objects.filter(list=list_)
-    return render(request,'list.html',{'list':list_})
 
 def new_list(request):
-    list_=List.objects.create()
-    
-    Item.objects.create(text=request.POST['item_text'],list=list_)
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
 
-def add_item(request,list_id):
-    list_=List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'],list=list_)
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
+
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
