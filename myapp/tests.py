3aeae508b7885ea610e1f918b01d9408b6e1307a
qmaowu <it_accp@126.com>
2020-05-24 12:57:00 +0000

    Hack FT runner to be able to test staging

--- myapp/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.urls import resolve
-from myapp.views import home_page
-# Create your tests here.
-class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found=resolve('/')
-        self.assertEqual(found.func,home_page)
