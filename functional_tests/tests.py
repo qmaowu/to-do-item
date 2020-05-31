3d75279728687267440e0b8328bca6b8b2e0ef71
qmaowu <it_accp@126.com>
2020-05-31 14:51:36 +0000

    use env vars for prod settings DEBUG,ALLOWED_HOSTS,SECRET_KEY

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -147,14 +147,14 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_layout_and_styling(self):
         #Edith goes to the home page
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024,768)
+        self.browser.set_window_size(1366,768)
 
 
         #She notices the input box is nicely centered
         inputbox=self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
                 inputbox.location['x']+inputbox.size['width']/2,
-                512,
+                683,
                 delta=10
         )
         #She starts a new list and sees the input is nicely
--- superlists/settings.py
+++ superlists/settings.py
@@ -23,9 +23,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i2ln&sh#3(gd6(mw*xyx8(9*jyeoq18rqyn5gle%n(0dl*#o2b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if 'DJANGO_DEBUG_FALSE' in os.environ:  
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']  
+    ALLOWED_HOSTS = [os.environ['SITENAME']]  
+else:
+    DEBUG = True  
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
 
 
 # Application definition
